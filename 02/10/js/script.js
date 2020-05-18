8a8da974aac4734074ec23a5cbc5f574e220ffac
Maria <mashatop@gmail.com>
2020-05-18 14:50:25 +0300

    Second commit

---
+++ 02/10/js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let incr = 10,
+    decr = 10;
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(incr);
+// console.log(decr);
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4 == 8);
+console.log(2*4 === 8); //Строгое равенство
+
+// && И
+// || ИЛИ
+// ! отрицание
+// != не равно
+// ++ увеличение на 1
+// -- уменьшение на 1
